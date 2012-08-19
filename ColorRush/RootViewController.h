d3189463b6b7ced52424544d76461d1e42f9fe8e
Hank CP <true.cp@gmail.com>
2012-08-19 18:03:14 +0800

    initial commit

---
+++ ColorRush/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  ColorRush
+//
+//  Created by Hank Zhu on 8/19/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
